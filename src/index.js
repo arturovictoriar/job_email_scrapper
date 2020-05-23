529879a9a7bf60fceb823d736ce62b99f58d9364
arvicrin <1231@holbertonschool.com>
2020-05-23 18:01:35 -0500

    make objects and split funcitons

--- src/index.js
+++ src/index.js
@@ -2,9 +2,36 @@ const unmejorempleo = require('./unmejorempleo');
 const C = require('./constants');
 
 (async () => {
-  const miEmpleo = new unmejorempleo.MejorEmpleo();
-  await miEmpleo.startBrowser(true, true);
-  await miEmpleo.login(C.username, C.password);
-  await miEmpleo.gotoVacantesPublicadas();
-  await miEmpleo.closeBrowser();
+  for (let i = 0; i < 2; i++) {
+    const miEmpleo = new unmejorempleo.MejorEmpleo();
+    await miEmpleo.startBrowser(true, true);
+    await miEmpleo.login(C.username, C.password);
+    const isAvailable = await miEmpleo.gotoVacantesPublicadas();
+    if (!isAvailable) {
+      process.exit();
+    }
+    const jobsName = await miEmpleo.getNamesJobs();
+    const JobOffers = await unmejorempleo.MejorEmpleo.appendSelectorNameJobs(jobsName);
+    const allEmails = JSON.parse(JSON.stringify(JobOffers));
+    for (const jobOffer in JobOffers) {
+      if (jobOffer) {
+        allEmails[jobOffer].shift();
+        const isAlive = await miEmpleo.gotoReviewApplication(JobOffers[jobOffer]);
+        if (!isAlive) {
+          await miEmpleo.returnVacantesAplicadas();
+          // eslint-disable-next-line no-continue
+          continue;
+        }
+        const basicInfoCandidate = await miEmpleo.getBasicInfoCandidate();
+        const allCurriculumSelector = await unmejorempleo.MejorEmpleo.curriculumSelectors(
+          basicInfoCandidate
+        );
+        const emailOnejob = await miEmpleo.getEmails(allCurriculumSelector, [], 0);
+        allEmails[jobOffer].push(...emailOnejob);
+        await miEmpleo.returnVacantesAplicadas();
+      }
+    }
+    console.log(allEmails, i);
+    await miEmpleo.closeBrowser();
+  }
 })();
